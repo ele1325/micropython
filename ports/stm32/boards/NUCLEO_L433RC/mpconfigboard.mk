179822480c2686770e0d85315bb1f794197e4f0b
jimmylee <ele1325@hotmail.com>
2022-08-24 14:12:30 +0800

    0824 ADC works

---
+++ ports/stm32/boards/NUCLEO_L433RC/mpconfigboard.mk
@@ -0,0 +1,15 @@
+MCU_SERIES = l4
+CMSIS_MCU = STM32L432xx
+AF_FILE = boards/NUCLEO_L433RC/stm32l433_af.csv
+LD_FILES = boards/NUCLEO_L433RC/stm32l433.ld boards/common_basic.ld
+OPENOCD_CONFIG = boards/openocd_stm32l4.cfg
+
+# MicroPython settings
+MICROPY_VFS_FAT = 1
+MICROPY_VFS_LFS1 ?= 0
+
+# Don't include default frozen modules because MCU is tight on flash space
+FROZEN_MANIFEST ?=
+
+# LTO reduces final binary size, may be slower to build depending on gcc version and hardware
+LTO ?= 1
